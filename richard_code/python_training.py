e8bdd544e3cdd337d481c21c60b7e953dc418d41
Richard Asiamah <123144205+Rich-Machine@users.noreply.github.com>
2024-10-16 15:34:49 -0400

    Update python_training.py
    Training accuracy = 94%

--- richard_code/python_training.py
+++ richard_code/python_training.py
@@ -1,6 +1,3 @@
-##Installing packages in VS Code
-# python3 -m venv env
-# source env/bin/activate
 import tensorflow as tf
 import numpy as np
 import mat73
@@ -9,7 +6,11 @@ from tensorflow.keras.utils import to_categorical
 from sklearn.model_selection import train_test_split
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Flatten, Dense, Dropout
+from tensorflow.keras.callbacks import EarlyStopping
 
+# Set random seed
+tf.random.set_seed(42)
+np.random.seed(42)
 params = {
     'dropout': 0.25,
     'batch-size': 128,
@@ -37,46 +38,70 @@ nodal_voltages = data["netloadV"]
 nodal_p = data["netloadP"]
 nodal_q = data["netloadQ"]
 
-nodal_voltages = nodal_voltages.reshape(48320160,)
-nodal_p = nodal_p.reshape(48320160,)
-nodal_q = nodal_q.reshape(48320160,)
+nodal_p = np.transpose(nodal_p)
+nodal_q = np.transpose(nodal_q)
+nodal_voltages = np.transpose(nodal_voltages)
 
-x = np.concatenate((nodal_p, nodal_q, nodal_voltages), axis=0)
-print(x.shape)
-x = x.reshape(48320160, 3)
-print(x.shape)
+nodal_voltages = nodal_voltages.reshape(48320160,1)
+nodal_p = nodal_p.reshape(48320160,1)
+nodal_q = nodal_q.reshape(48320160,1)
 
+x = np.concatenate((nodal_p, nodal_q, nodal_voltages), axis=1)
+# x= np.transpose(x)
+print(x.shape)
+# x = x.reshape(503335, 288)
+# print(x.shape)
+
+# Assuming each sample should be 96 timesteps long
+timesteps = 96
+num_samples = x.shape[0] // timesteps  # Number of samples with 96 timesteps each
+
+x = x[:num_samples * timesteps]  # Trim the data to fit exactly into (num_samples, 96, 3)
+x = x.reshape(num_samples, timesteps, 3)
+print("New input shape:", x.shape)
+
+# One-hot encoding the target variable
 y = to_categorical(one_hot_vector)
-y = np.repeat(y, 35040, axis=0)
-print(y.shape)
+y = np.repeat(y, 365, axis=0)
+y = y.reshape(num_samples, 1, 4)
+print("New target shape:", y.shape)
+# y = np.repeat(y, 365, axis=0)
+# # y = np.repeat(y, 35040, axis=0)
+# print(y.shape)
 
+# Splitting the reshaped data
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+# Remove the extra dimension in the target labels
+y_train = y_train.reshape(-1, 4)
+y_test = y_test.reshape(-1, 4)
 
 # Model Definition
 # Get parameters from logged hyperparameters
-model = Sequential([
-  Flatten(input_shape=(3, )),
-  Dense(128, activation='relu'),
-  Dense(64, activation='relu'),
-  Dropout(0.25),
-  Dense(4, activation='softmax')
-  ])
+model = Sequential()
+model.add(Dense(100, activation='relu', input_shape=(96, 3)))  # Input: (96, 3)
+model.add(Dropout(0.25))
+model.add(Flatten())  # Flatten the output before the final Dense layer
+model.add(Dense(4, activation='softmax')) 
 
 loss_fn = tf.keras.losses.CategoricalCrossentropy(from_logits=True)
+early_stopping = EarlyStopping(monitor='val_loss', patience=3)
 
 model.compile(optimizer='adamax', 
               loss=loss_fn,
               metrics=['accuracy'])
 
 model.fit(x_train, y_train,
-    batch_size=50,
-    epochs=50,
-    validation_data=(x_test, y_test),)
+          batch_size=50,
+          epochs=50,
+          validation_data=(x_test, y_test),
+          callbacks=[early_stopping])
 
 score = model.evaluate(x_test, y_test)
 predicts = model.predict(x_test)
-print(predicts)
-print(y_test)
-print('Test loss:', score[0])
-print('Test accuracy:', score[1])
+
+rounded_predicts = np.round(predicts)
+accuracy = np.mean(rounded_predicts == y_test)
+error = np.mean(rounded_predicts != y_test)
+print('Accuracy: {:.2f}%'.format(accuracy * 100))
+
 
